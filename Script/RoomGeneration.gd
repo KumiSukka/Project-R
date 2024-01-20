5936aaf33e114847f365e716d1db8b625b4891a7
Mika.Lukkarinen <Mika.lukkarinen0@gmail.com>
2024-01-20 10:58:16 +0200

    Projekti tähän saakka
    puuttuu madolliset aikaisemmat committi tekstit en päässyt toiselle git tilillel sisään joten tehdään sitten uudella

---
+++ Script/RoomGeneration.gd
@@ -0,0 +1,54 @@
+extends Node
+#Tämä osio toimii oksana
+class_name Branch
+
+var left_child: Branch
+var right_child: Branch
+var position: Vector2
+var size: Vector2i
+
+
+func _init(position, size):
+	self.position = position
+	self.size = size
+	
+func get_leaves():
+	if not(left_child && right_child):
+		return [self]
+	else:
+		return left_child.get_leaves() + right_child.get_leaves()
+
+func get_center():
+	return Vector2i(position.x + size.x /2, position.y + size.y/2)
+
+func split(remaining, paths):
+	var rng = RandomNumberGenerator.new()
+	var split_percent = rng.randf_range(0.35,0.65) #Oksien katkaisut tulee olemaan 30%-70%
+	var split_horizontal = size.y >= size.x #tarkastetaan että ei ole korkeampi kuin leveä
+
+	if(split_horizontal):
+		#horizontal - vaakasuora
+		var left_height = int(size.y * split_percent)
+		left_child = Branch.new(position, Vector2i(size.x, left_height))
+		right_child = Branch.new(
+			Vector2i(position.x, position.y + left_height),
+			Vector2i(size.x, size.y - left_height)
+		)
+	else:
+		#Vertical - Pystysuora
+		var left_width = int(size.x * split_percent)
+		left_child = Branch.new(position, Vector2i(left_width, size.y))
+		right_child = Branch.new(
+			Vector2i(position.x + left_width, position.y),
+			Vector2i(size.x - left_width, size.y)
+		)
+	if(remaining > 0):
+		left_child.split(remaining - 1, paths)
+		right_child.split(remaining - 1, paths)
+	paths.push_back({'left': left_child.get_center(), 'right': right_child.get_center()})
+	pass
+# We are trying to keep everything as ints, this is so that it makes it easier to swap out each 1x1 for a tile in our tilemap later.
+# We recursively split cells until remaining is zero. This means we only have to call split(5) on the root node, and it will give us 32 rooms.
+
+
+	#https://jonoshields.com/post/bsp-dungeon Tämän hienon dugemention bsp dungeonista mukaan täystetty systeemin pohja, koska kävi läpi godotissa ja myöskin miten mapata autotilet, jonka kanssa minulla on suuresti ollut ongelmia
---
+++ Script/node_2d.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+var root_node: Branch
+var tilemap:TileMap
+var tile_size: int = 16
+var paths: Array = []
+var cells: Array = []
+
+
+
+
+#Tässä piirämme lehdet
+func _ready():
+	tilemap = get_node("TileMap")
+	root_node  = Branch.new(Vector2i(0, 0), Vector2i(150, 80)) #60 leveä ja 30 pitkä
+	root_node.split(4, paths)
+	queue_redraw()
+	pass
+
+func _draw():
+	var rng = RandomNumberGenerator.new()
+	for leaf in root_node.get_leaves():
+		var padding = Vector4i(
+			rng.randi_range(1,3),
+			rng.randi_range(1,3),
+			rng.randi_range(1,3),
+			rng.randi_range(1,3)
+			)
+		var padding2 = Vector4i(0,0,0,0)
+		
+		for x in range(leaf.size.x):
+			for y in range(leaf.size.y):
+				if not is_outside_padding(x, y, leaf, padding):
+					tilemap.set_cell(-1, Vector2i(x + leaf.position.x  +1,y + leaf.position.y +1) , 0, Vector2i(17, 8))
+				if not is_inside_padding(x, y, leaf, padding):
+					tilemap.set_cell(0, Vector2i(x + leaf.position.x,y + leaf.position.y), 0, Vector2i(9, 12))
+					draw_rect(
+			Rect2(
+				leaf.position.x * tile_size, # x
+				leaf.position.y * tile_size, # y
+				leaf.size.x * tile_size, # width/leveys
+				leaf.size.y * tile_size # height/korkeus
+			), 
+			Color.REBECCA_PURPLE, # colour/väri reunille
+			true # is filled - täytetty
+			)
+	for path in paths:
+		if path["left"].y == path["right"].y:
+			#horizontal
+				for i in range(path["right"].x - path["left"].x):
+					tilemap.set_cell(0, Vector2i(path["left"].x+i,path["left"].y), 0, Vector2i(9, 12))
+		else:
+		 	# vertical
+				for i in range(path['right'].y - path['left'].y):
+					tilemap.set_cell(0, Vector2i(path['left'].x,path['left'].y+i), 0, Vector2i(9, 12))
+	pass
+
+func is_inside_padding(x, y, leaf, padding):
+	return x <= padding.x +2 or y <= padding.y +2 or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w
+func is_outside_padding(x, y, leaf, padding):
+	return x <= padding.x or y <= padding.y or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w
+	
+
