252890c9e4437c5c6735e6aecee504d93a19b9b1
Mika.Lukkarinen <Mika.lukkarinen0@gmail.com>
2024-01-24 17:04:29 +0200

    Fixed collissions on walls, simple player movement to test collissions

--- Prefab/Character/Kunoichi.gd
+++ Prefab/Character/Kunoichi.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
 
-func _physics_process(_delta):
-	
+@export var speed = 200
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
