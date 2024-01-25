581f346fc6df5ca83b3142719f4be87565f44cf9
Mika.Lukkarinen <Mika.lukkarinen0@gmail.com>
2024-01-25 11:33:36 +0200

    Working walking animations

--- Prefab/Character/Kunoichi.gd
+++ Prefab/Character/Kunoichi.gd
@@ -6,12 +6,14 @@ extends CharacterBody2D
 
 @onready var animation_tree = $AnimationTree
 @onready var Player_Sprite = $PlayerSprite
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
 	update_animation_parameters(starting_direction)
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	update_animation_parameters(input_direction)
 	velocity = input_direction * speed
 	#Flip
 	if input_direction == Vector2.RIGHT:
@@ -19,16 +21,21 @@ func get_input():
 	elif input_direction == Vector2.LEFT:
 		Player_Sprite.flip_h = true
 
-	update_animation_parameters(input_direction)
+	
 	
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	pick_state()
 
 func update_animation_parameters(move_input : Vector2):
 	#Elä vaihda animaatio parameettrejä jos ei imputtia liikkua
 	if (move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
-
+func pick_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
