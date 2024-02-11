b71274365ef8f2924c0cd37ce58faf42e4050368
Mika.Lukkarinen <Mika.lukkarinen0@gmail.com>
2024-02-11 16:17:25 +0200

    Flip logick to enemy

--- Prefab/Character/Enemy/karakaza_kozo.gd
+++ Prefab/Character/Enemy/karakaza_kozo.gd
@@ -4,12 +4,21 @@ extends CharacterBody2D
 var speed = 30
 var player_chase = false
 var player = null
+@onready var animationTree = $AnimationTree
+@onready var enemy = $Sprite2D
 
 func _physics_process(delta):
 	if player_chase:
+		#add here logick to chanse to chase animation state
 		position += (player.position - position)/speed
 		
-		
+		if(player.position.x - position.x) < 0:
+			enemy.flip_h = true
+		else:
+			enemy.flip_h = false
+	else:
+		#Add here logick to change to idle
+			pass
 
 func _on_detection_area_body_entered(body):
 	player = body
