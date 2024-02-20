9af5d8493df2fcf0545cebf67ab534b12a5cdd31
Mika.Lukkarinen <Mika.lukkarinen0@gmail.com>
2024-02-20 16:15:20 +0200

    timer to despawn for testing

--- Prefab/spawner.gd
+++ Prefab/spawner.gd
@@ -1,12 +1,18 @@
 extends Area2D
 
 var enemy1 := preload("res://Prefab/Character/Enemy/karakaza_kozo.tscn")
-
+@onready var timer = $SpawnerDisable
 
 func _on_timer_timeout():
 	monitoring = true
 
 
 func _on_body_entered(body):
-	queue_free()
+	monitoring = false
+	timer.start()
+	
 
+
+
+func _on_spawner_disable_timeout():
+	queue_free()
